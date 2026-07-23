c86fabfa8c6cb464f48c6bdeb51abf88c4311bfe
ojace8143 <ojace8143@gmail.com>
2026-07-23 15:41:50 -0400

    added pvp spawn protection

---
+++ src/main/java/com/ojace/spawnprotect/PvPProtection.java
@@ -0,0 +1,33 @@
+package com.ojace.spawnprotect;
+
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
+
+public class PvPProtection {
+
+    private static final int PROTECTION_RADIUS = 100;
+
+    public static void register() {
+
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+
+            if (!(entity instanceof Player)) {
+                return InteractionResult.PASS;
+            }
+
+            BlockPos spawn = world.getSharedSpawnPos();
+
+            double distance = player.position().distanceTo(
+                    spawn.getCenter()
+            );
+
+            if (distance <= PROTECTION_RADIUS) {
+                return InteractionResult.FAIL;
+            }
+
+            return InteractionResult.PASS;
+        });
+    }
+}
--- src/main/java/com/ojace/spawnprotect/SpawnProtect.java
+++ src/main/java/com/ojace/spawnprotect/SpawnProtect.java
@@ -7,5 +7,7 @@ public class SpawnProtect implements ModInitializer {
     @Override
     public void onInitialize() {
         System.out.println("[SpawnProtect] Loaded successfully!");
+
+	PvPProtection.register();
     }
 }
