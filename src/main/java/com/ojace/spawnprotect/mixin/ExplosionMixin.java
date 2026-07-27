be6fb3acf6ca700e034a711f6472ecc41ec789cf
ojace8143 <ojace8143@gmail.com>
2026-07-27 17:23:51 -0400

    fixed mixin bug, first functional prototype. PvP disabled in a 100 block radius of spawn.

---
+++ src/main/java/com/ojace/spawnprotect/mixin/ExplosionMixin.java
@@ -0,0 +1,9 @@
+package com.ojace.spawnprotect.mixin;
+
+import net.minecraft.world.level.Explosion;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Explosion.class)
+public class ExplosionMixin {
+
+}
